5fb841ffed4e72791bcec1fc06a0284f4e9a37be
Shadman-Shakib-Fuad <shadmanfuad456@gmail.com>
2026-03-08 14:00:00 +0600

    login js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 let issues=[]
+
+let issues=[]
+
+function login(){
+
+let user=document.getElementById("username").value
+let pass=document.getElementById("password").value
+
+if(user==="admin" && pass==="admin123"){
+
+document.getElementById("loginPage").classList.add("hidden")
+document.getElementById("mainPage").classList.remove("hidden")
+
+loadIssues()
+
+}else{
+alert("Wrong Credentials")
+}
+
+}
